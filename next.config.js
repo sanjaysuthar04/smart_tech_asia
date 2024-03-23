92161dfbf62c2426a3dd633edad1a91c135c45f4
Sanjay Suthar <sanjay.suthar@apiindia.com>
2024-03-24 00:14:48 +0530

    next-config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://astro-paper-4qz.pages.dev/posts", //-> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://astro-paper-4qz.pages.dev/posts/:path*",// -> Replace https://starter-kit-rose-seven.vercel.app with your own Vercel deployment URL from step 1
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
